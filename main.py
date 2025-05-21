0e925a8a8d6a40abf3c33493b8ab881b7658d1e8
DovSchwartz <schwartzdov75@gmai.com>
2025-05-20 22:33:17 -0700

    fixed env file tracking

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from langchain_core.output_parsers import PydanticOutputParser
 from langchain.agents import create_tool_calling_agent, AgentExecutor
 from tools import search_tool, wiki_tool, save_tool
 
-load_dotenv(dotenv_path='sample.env')
+load_dotenv()
 
 class ResearchResponse(BaseModel):
     topic: str
